0681bb4faf04e04e00e693701e0b10d15d88fa93
svasilchenko <liketravelin@gmail.com>
2023-05-02 20:33:38 +0300

    fix assertions and add print for kwargs

--- test.py
+++ test.py
@@ -29,14 +29,14 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = True
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
-    if dark_theme_enabled_by_user == None:
+    if dark_theme_enabled_by_user is None:
         if time(hour=6) < current_time < time(hour=22):
             is_dark_theme = False
         else:
             is_dark_theme = True
-    if dark_theme_enabled_by_user == True:
+    if dark_theme_enabled_by_user is True:
         is_dark_theme = True
-    elif dark_theme_enabled_by_user == False:
+    elif dark_theme_enabled_by_user is False:
         is_dark_theme = False
 
     assert is_dark_theme is True
@@ -93,17 +93,21 @@ def test_readable_function():
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
 
+
 def open_browser(browser_name):
     actual_result = get_readable_function(func=open_browser, browser_name=browser_name)
+    print(f'\n{actual_result}\n')
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
     actual_result = get_readable_function(go_to_companyname_homepage, page_url=page_url)
+    print(f'\n{actual_result}\n')
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
     actual_result = get_readable_function(find_registration_button_on_login_page, page_url=page_url,
                                           button_text=button_text)
+    print(f'\n{actual_result}\n')
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
